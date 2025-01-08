f771fc041a5e11f0d1e5c92598c9cbf6f6673ae8
Adrian Quinonez <adrianquinonezon@unimagdalena.edu.co>
2025-01-07 21:29:18 -0500

    refactor: :zap: Ajustando el hook

--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -1,28 +1,50 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const useHttp = (url) => {
+const useHttp = (initialUrl) => {
+  const [url, setUrl] = useState(initialUrl);
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
-  const fetchData = async (method = 'GET', body = null) => {
+  const fetchData = async (method = 'GET', body = null, headers = {}) => {
     setLoading(true);
+    setError(null); 
+    const controller = new AbortController();
+    const signal = controller.signal;
+
     try {
       const response = await axios({
         method,
         url,
         data: body,
+        headers,
+        signal,
       });
       setData(response.data);
     } catch (err) {
-      setError(err.message);
+      if (axios.isCancel(err)) {
+        console.log('Request canceled', err.message);
+      } else {
+        setError(err.message);
+      }
     } finally {
       setLoading(false);
     }
+
+    return () => {
+    controller.abort();
+    };
   };
 
-  return { data, error, loading, fetchData };
+  useEffect(() => {
+    return () => {
+    const controller = new AbortController();
+    controller.abort();
+    };
+  }, []);
+
+  return { data, error, loading, fetchData, setUrl };
 };
 
 export default useHttp;
